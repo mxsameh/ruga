f2b44f7f4fb3acbb0b5de049b9722661c211fdce
Mohamed Sameh <mxsameh@gmail.com>
2026-05-11 18:33:46 +0300

    s

---
+++ assets/a-main.js
@@ -0,0 +1,138 @@
+const masterTl = gsap.timeline();
+
+const init = () => {
+  gsap.set("#_", {
+    autoAlpha: 1,
+  });
+};
+
+const hA = () => {
+  const tl = gsap.timeline();
+
+  tl.set("#h", { background: "white" })
+    .from("#h .h_l .y", {
+      yPercent: 100,
+      duration: 0.8,
+      ease: "power2.inOut",
+    })
+    .from(
+      "#h .h_r .y",
+      {
+        yPercent: 100,
+        duration: 0.8,
+        ease: "sine.inOut",
+        stagger: 0.2,
+      },
+      "<",
+    );
+
+  return tl;
+};
+
+const loA = () => {
+  const tl = gsap.timeline();
+
+  tl.from("#lo .y", {
+    yPercent: 100,
+    duration: 1.2,
+    ease: "power2.inOut",
+    stagger: 0.2,
+  })
+    .to("#lo .lo_bg", {
+      delay: 1,
+      yPercent: -100,
+      duration: 1.5,
+      ease: "power3.inOut",
+    })
+    .to(
+      "#lo .y",
+      {
+        yPercent: -100,
+        duration: 0.6,
+        ease: "power2.inOut",
+        //   stagger: 0.2,
+      },
+      "<0.2",
+    )
+    .set("#lo", { autoAlpha: 0 });
+  return tl;
+};
+
+const heA = () => {
+  // const wh = window.innerHeight  ;
+  // const hh = 72;
+  // const vh = wh - 72 - 40;
+  const tl = gsap.timeline();
+
+  tl.from("#he-ban", {
+    opacity: 0,
+    duration: 0.6,
+    ease: "power3.inOut",
+  })
+    .from(".s-hero", {
+      duration: 1.2,
+      padding: 0,
+      ease: "power3.inOut",
+    })
+    .from(
+      "#he-ban",
+      {
+        width: "100%",
+        height: "100%",
+        borderRadius: "0",
+        duration: 1.2,
+        ease: "power3.inOut",
+      },
+      "<",
+    );
+  return tl;
+};
+
+const colcA = () => {
+  gsap.from(".s-colc h2 .y", {
+    yPercent: 100,
+    duration: 1,
+    ease: "sine.inOut",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".s-colc",
+      start: "top 80%",
+    },
+  });
+  gsap.from(".s-colc ul li", {
+    yPercent: 100,
+    duration: 0.8,
+    ease: "power2.inOut",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: ".s-colc ul",
+      start: "top 80%",
+    },
+  });
+};
+
+const lifeA = () => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".s-life",
+      start: "top 80%",
+    },
+  });
+  tl.from(".s-life figure ", {
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1,
+    ease: "sine.inOut",
+  })
+  
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await document.fonts.ready;
+  init();
+  lifeA();
+  //   heA();
+  //   hA();
+  //   masterTl.add(loA()).add(heA()).add(hA());
+  //   colcA();
+});
