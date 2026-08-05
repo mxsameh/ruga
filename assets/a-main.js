7ad3d1da507d76d7f3e24a77f59d57f445a5545a
Mohamed Sameh <mxsameh@gmail.com>
2026-08-05 19:00:06 +0300

    s

--- assets/a-main.js
+++ assets/a-main.js
@@ -265,7 +265,8 @@ const animation = async () => {
     // - OR user refreshed page
     if (!hasPlayed || isReload) {
       sessionStorage.setItem("loaderPlayed", "true");
-      masterTl.add(loA()).add(heA()).add(hA(), "-=0.8");
+      masterTl.add(loA()).add(hA());
+      // masterTl.add(loA()).add(heA()).add(hA(), "-=0.8");
     } else {
       // normal page-to-page navigation
       window.lenis.start();
@@ -279,7 +280,7 @@ const animation = async () => {
   }
 
   if (isMobile) {
-    heAM();
+    // heAM();
     colcAM();
   }
 
